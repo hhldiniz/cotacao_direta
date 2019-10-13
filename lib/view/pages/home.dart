a0ac2824d0b08212a6f68ba745bb53ae81227b21
hugo <hhldiniz@gmail.com>
2019-10-13 16:43:59 -0300

    alteração no layout da página principal

--- lib/view/pages/home.dart
+++ lib/view/pages/home.dart
@@ -1,14 +1,28 @@
-import 'package:flutter/material.dart';
 import 'package:cotacao_direta/view/widgets/dollar_exchange_rate.dart';
+import 'package:cotacao_direta/view/widgets/euro_exchange_rate.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget{
+
+  final dollarExchangeRate = DollarExchangeRate();
+  final euroExchangeRate = EuroExchangeRate();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Column(
           children: <Widget>[
-            DollarExchangeRate()
+            Row(
+              children: <Widget>[
+                dollarExchangeRate
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                euroExchangeRate
+              ],
+            )
           ],
         ),
       ),
