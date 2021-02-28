680a0fc98e9d309fb0912cda39468154d143f191
Hugo <hhldiniz@gmail.com>
2021-02-28 20:50:26 -0300

    chore: Alterando tipo do identificador de configurações para uma variável no lugar de constante

--- lib/dao/configuration_dao.dart
+++ lib/dao/configuration_dao.dart
@@ -3,7 +3,7 @@ import 'package:cotacao_direta/util/database.dart';
 import 'package:sqflite/sqflite.dart';
 
 class ConfigurationDao {
-  static const configurationId = 1;
+  static int configurationId = 1;
 
   Future<void> insert(Configuration configuration) async {
     final Database db = await AppDatabase().openAppDatabase();
